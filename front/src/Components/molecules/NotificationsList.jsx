39b543301877a86b4f5b1f67b896c6d751735038
jusa11 <a.juiceit@gmail.com>
2025-10-05 00:19:46 +0500

    Edit axios query

--- front/src/Components/molecules/NotificationsList.jsx
+++ front/src/Components/molecules/NotificationsList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+
 const NotificationsList = ({
   isShowNotifications,
   setIsShowNotifications,
